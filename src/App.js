218e10b0656bff99cc19aaae28081319af6b34e0
apache-0002 <vinyy64@live.com>
2021-08-27 20:38:58 -0300

    version1

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import { userSchema } from "./Validations/UserValidation";
 
 
 export default function App() {
-  const createUser = (event) => {
+  const createUser = async (event) => {
     	event.preventDefault()
       let formData = {
         name: event.target[0].value,
         email: event.target[1].value,
         password: event.target[2].value,
       };
-      console.log(formData);
+      const isValid = await userSchema.isValid(formData)
+      console.log(isValid)
   }
 
   return (
